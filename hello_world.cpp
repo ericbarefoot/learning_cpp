d1b51a4af553a5b94115be847f4f788ced006803
Eric Barefoot <eabarefoot@gmail.com>
2019-09-13 17:24:01 -0400

    initial commit

---
+++ hello_world.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+
+int main()
+{
+  char hello[] = "Hello World!\n";
+  char x;
+  std::cout << hello << x;
+  return 0;
+}
+
+/*
+text
+some lines in here
+here a line
+*/
+
+// ssome other lines
+// ssome other lines
+// ssome other lines
+// ssome other lines
+// ssome other lines
+// ssome other lines
+
+// another line
